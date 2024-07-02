d7a3d995c74f752cc118094e2dab70c338a78f31
amanc1248 <amanc1248@gmail.com>
2024-07-02 20:54:01 +0545

    passed event emitters

--- nodejspractice/eventEmitter.js
+++ nodejspractice/eventEmitter.js
@@ -1,10 +1,27 @@
 const EventEmitter = require("events");
+const util = require("util");
 const emitter = new EventEmitter();
 
 // define an event listener for a custom event
-emitter.on("customEvent", (message)=>{
-    console.log(`Received message ${message}`);
+emitter.on("customEvent", (message) => {
+  console.log(`Received message ${message}`);
 });
 
 // Emit the  custom event
-emitter.emit("customEvent",'Hello world');
+setTimeout(() => emitter.emit("customEvent", "Hello world"), 3000);
+
+// using util 
+var Person = function(name){
+    this.name = name;
+};
+util.inherits(Person, EventEmitter);
+var james = new Person("james");
+var ryu = new Person("ryu");
+var mary = new Person("mary");
+var people = [james, ryu, mary];
+people.forEach((person)=>{
+    person.on("speak",(message)=>{
+        console.log(`${person.name} said: ${message}`);
+    })
+});
+james.emit("speak","I am going to home")
